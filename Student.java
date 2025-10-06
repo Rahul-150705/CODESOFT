9bc012e39cf2c3653ed9975951ac0b3138989ff2
Rahul P <rahul.cd23@bitsathy.ac.in>
2025-10-06 23:47:04 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,15 @@
+package Student;
+
+public class Student {
+	int id;
+	String name;
+	int totalmark;
+	public Student(int id, String name, int totalmark) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.totalmark = totalmark;
+	}
+
+}
+
